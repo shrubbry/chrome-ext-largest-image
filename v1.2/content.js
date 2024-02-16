cadda8bfb9a14e788d1f7dc07357e0cf36371abf
shrubbry <shrubbry@gmail.com>
2024-02-15 18:22:50 -0800

    Update content.js

--- v1.2/content.js
+++ v1.2/content.js
@@ -12,7 +12,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         blankImageCount++;
       }
       else if (displayedSize > largestDisplayedSize) {
-        // Otherwise compare to  current largest
+        // Otherwise compare to "current" largest
         largestDisplayedSize = displayedSize;
         largestImage = img.src;
       }
@@ -24,7 +24,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
       // Open the largest displayed image in the same tab
       window.open(largestImage, '_self');
     } else {
-      // No images found or only a single image, go back to the previous page
+      // If no images or only a single image, go back to the previous page
       window.history.back();
     }
   }
